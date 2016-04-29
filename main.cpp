7fbb06490eb09a6bf6e500880cecd0c42da711a1
Shaya Arshadi <shaya.arshadi@gmail.com>
2016-04-29 18:59:43 +0430

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*
+ * Simple program to use 'strrev' function in linux.
+ */
+#include <stdio.h>
+#include <string.h>
+
+char *strrev(char *str);
+int main()
+{
+    char arr[100];
+
+    printf("Enter a string to reverse\n");
+    gets(arr);
+
+    strrev(arr);
+
+    printf("Reverse of entered string is \n%s\n",arr);
+
+    return 0;
+}
+
+char *strrev(char *str)
+{
+    char *p1, *p2;
+
+    if (! str || ! *str)
+        return str;
+    for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+    {
+        *p1 ^= *p2;
+        *p2 ^= *p1;
+        *p1 ^= *p2;
+    }
+    return str;
+}
